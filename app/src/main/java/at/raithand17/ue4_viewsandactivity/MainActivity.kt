f9894d35ce2d7f0c28ae3fdae400ec027b8571fe
Andreas <andreas.raith@edu.fh-joanneum.at>
2018-12-05 09:19:57 +0100

    Init commit

---
+++ app/src/main/java/at/raithand17/ue4_viewsandactivity/MainActivity.kt
@@ -0,0 +1,12 @@
+package at.raithand17.ue4_viewsandactivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
